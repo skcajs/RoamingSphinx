945ffa69e5743d02e13fc406a2e804c9eef9a87a
samj <sjackson864@outlook.com>
2023-03-15 23:40:52 +0100

    started doing the gene selection

--- Program.cs
+++ Program.cs
@@ -5,62 +5,93 @@
         static void Main(string[] args)
         {
             FitnessFunction fitness = new BasicFitnessFunction();
-            Population pop = new Population(100);
+            Population pop = new Population(100, 16);
             GA ga = new GA(pop, fitness);
-            // ga.sort();
+            ga.Fitness();
+            Console.WriteLine(ga.records.Count);
+            // foreach(FitnessRecord record in ga.records) {
+            //     Console.WriteLine(string.Join("", record.Gene));
+            //     Console.WriteLine(record.Fitness);
+            //     Console.WriteLine("\n");
+            // }
         }
+    }
+
+    public struct FitnessRecord
+    {
+        public FitnessRecord(int[] gene, int fitness)
+        {
+            Gene = gene;
+            Fitness = fitness;
+        }
+
+        public int[] Gene { get; }
+        public int Fitness { get; }
     }
 
     class GA
     {
-        Population population;
-        FitnessFunction fitness;
+        private Population population;
+        private FitnessFunction fitnessFunction;
+        public List<FitnessRecord> records { get;}
 
-        public GA(Population population, FitnessFunction fitness) {
+        public GA(Population population, FitnessFunction fitnessFunction) {
             this.population = population;
-            this.fitness = fitness;
+            this.fitnessFunction = fitnessFunction;
+            this.records = new List<FitnessRecord>();
         }
 
-        public void InitPopulation(int popSize, int geneSize) {
-            for (int i = 0; i < popSize; i++) {
-                this.population.generateGenes(geneSize);
+        public void Fitness() {
+            records.Clear();
+            foreach (int[] gene in this.population.genes) {
+                this.records.Add(new FitnessRecord(gene, fitnessFunction.CalculateFitness(gene)));
             }
+            int nRecordsHalf = records.Count / 2;
+            this.records.Sort((x,y) => x.Fitness.CompareTo(y.Fitness));
+            this.records.RemoveRange((nRecordsHalf - 1), nRecordsHalf);
         }
 
-        public void cost() {
-            
-        }
-
-        public void cross() {
+        public void Cross() {
 
         }
 
-        public void mutate() {
+        public void Mutate() {
 
         }
     }
 
     class Population
     {
-        private List<int[]> population;
+        public List<int[]> genes {get;}
 
-        public Population(int size) {
-            this.population = new List<int[]>();
+        public Population(int popSize, int geneSize) {
+            this.genes = new List<int[]>();
+            Init(popSize, geneSize);
         }
 
-        public void generateGenes(int size) {
+        public void Init(int popSize, int geneSize) {
+            for (int i = 0; i < popSize; i++) {
+                generateGenes(geneSize);
+            }
+        }
+
+        private void generateGenes(int size) {
             Random rn = new Random();
             int[] pop = new int[size];
             for(int i = 0; i < size; i++) {
                 pop[i] =  rn.Next() % 2;
             }
-            this.population.Add(pop);
+            this.genes.Add(pop);
+        }
+
+        public void updateGenes() {
+            
         }
     }
 
     abstract class FitnessFunction
     {
-        public abstract int CalculateFitness(int[] genes);
+        public abstract int CalculateFitness(int[] gene);
     }
 
     class BasicFitnessFunction : FitnessFunction
