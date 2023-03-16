38e85cdd67df6d5565934d50a140a70d413a531d
skcajs <sjackson864@outlook.com>
2023-03-16 19:17:46 +0100

    first pass

--- Program.cs
+++ Program.cs
@@ -7,13 +7,14 @@
             FitnessFunction fitness = new BasicFitnessFunction();
             Population pop = new Population(100, 16);
             GA ga = new GA(pop, fitness);
-            ga.Fitness();
-            Console.WriteLine(ga.records.Count);
-            // foreach(FitnessRecord record in ga.records) {
-            //     Console.WriteLine(string.Join("", record.Gene));
-            //     Console.WriteLine(record.Fitness);
-            //     Console.WriteLine("\n");
-            // }
+            for (int i = 0; i < 1000; i++)
+            {
+                ga.Fitness();
+                ga.Crossover();
+                ga.Mutate();
+            }
+            ga.Score();
+
         }
     }
 
@@ -33,59 +34,92 @@
     {
         private Population population;
         private FitnessFunction fitnessFunction;
-        public List<FitnessRecord> records { get;}
+        public List<FitnessRecord> records { get; }
+        private Random rn;
 
-        public GA(Population population, FitnessFunction fitnessFunction) {
+        public GA(Population population, FitnessFunction fitnessFunction)
+        {
             this.population = population;
             this.fitnessFunction = fitnessFunction;
             this.records = new List<FitnessRecord>();
+            this.rn = new Random();
         }
 
-        public void Fitness() {
+        public void Fitness()
+        {
             records.Clear();
-            foreach (int[] gene in this.population.genes) {
+            foreach (int[] gene in this.population.genes)
+            {
                 this.records.Add(new FitnessRecord(gene, fitnessFunction.CalculateFitness(gene)));
             }
             int nRecordsHalf = records.Count / 2;
-            this.records.Sort((x,y) => x.Fitness.CompareTo(y.Fitness));
+            this.records.Sort((x, y) => x.Fitness.CompareTo(y.Fitness));
             this.records.RemoveRange((nRecordsHalf - 1), nRecordsHalf);
+            this.population.genes = this.population.genes.Where(g => records.Any(a => a.Gene.Equals(g))).ToList();
         }
 
-        public void Cross() {
+        public void Crossover()
+        {
+            int crossPoint = (int)(this.population.shape.Item2 / 3);
+            int count = this.population.genes.Count / 2;
+            this.population.genes.OrderBy(a => this.rn.Next());
 
+            List<int[]> pool1 = this.population.genes.Take(count).ToList();
+            List<int[]> pool2 = this.population.genes.Skip(count).ToList();
+
+            for (int i = 0; i < count; ++i)
+            {
+                population.genes.Add(pool1[i].Take(crossPoint).Concat(pool2[i].Skip(crossPoint)).ToArray());
+                population.genes.Add(pool2[i].Take(crossPoint).Concat(pool1[i].Skip(crossPoint)).ToArray());
+            }
         }
 
-        public void Mutate() {
+        public void Mutate()
+        {
+            int count = this.population.genes.Count / 2;
+            for (int i = 0; i < count; ++i)
+            {
+                int mutateIndex = rn.Next() % this.population.shape.Item2;
+                bool mutateChromosone = !Convert.ToBoolean(this.population.genes[count + 1][mutateIndex]);
+                this.population.genes[count + 1][mutateIndex] = Convert.ToInt32(mutateChromosone);
+            }
+        }
 
+        public void Score()
+        {
+            Console.WriteLine(records);
         }
     }
 
     class Population
     {
-        public List<int[]> genes {get;}
+        public List<int[]> genes { get; set; }
+        public (int, int) shape { get; }
 
-        public Population(int popSize, int geneSize) {
+        public Population(int popSize, int geneSize)
+        {
+            this.shape = (popSize, geneSize);
             this.genes = new List<int[]>();
             Init(popSize, geneSize);
         }
 
-        public void Init(int popSize, int geneSize) {
-            for (int i = 0; i < popSize; i++) {
+        public void Init(int popSize, int geneSize)
+        {
+            for (int i = 0; i < popSize; i++)
+            {
                 generateGenes(geneSize);
             }
         }
 
-        private void generateGenes(int size) {
+        private void generateGenes(int size)
+        {
             Random rn = new Random();
             int[] pop = new int[size];
-            for(int i = 0; i < size; i++) {
-                pop[i] =  rn.Next() % 2;
+            for (int i = 0; i < size; i++)
+            {
+                pop[i] = rn.Next() % 2;
             }
             this.genes.Add(pop);
-        }
-
-        public void updateGenes() {
-            
         }
     }
 
@@ -98,13 +132,16 @@
     {
         int fitness;
 
-        public BasicFitnessFunction() {
+        public BasicFitnessFunction()
+        {
             fitness = 0;
         }
 
-        public override int CalculateFitness(int[] genes) {
+        public override int CalculateFitness(int[] genes)
+        {
             this.fitness = genes.Length;
-            foreach (int gene in genes) {
+            foreach (int gene in genes)
+            {
                 this.fitness = gene == 1 ? --this.fitness : this.fitness;
             }
             return this.fitness;
